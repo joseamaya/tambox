5657a410de7f2bd0162972ad4022d4155b4ea659
joseamaya <miguel.amaya99@gmail.com>
2016-10-07 10:18:30 -0500

    Corregido error que no mostraba datos del proveedor en la O.C

--- tambox/settings/base.py
+++ tambox/settings/base.py
@@ -103,14 +103,8 @@ SUIT_CONFIG = {
     'ADMIN_NAME': 'TAMBOX',
 }
 
-"""EMAIL_USE_TLS = True
+EMAIL_USE_TLS = True
 EMAIL_HOST = '#'
 EMAIL_HOST_USER = '#'
 EMAIL_HOST_PASSWORD = '#'
-EMAIL_PORT = 25"""
-
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'tambox.intix@gmail.com'
-EMAIL_HOST_PASSWORD = 'r3v0luc10n'
 EMAIL_PORT = 25
