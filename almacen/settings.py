ee6dad581dcb30cccadc2002b7bcd822781faac9
Miguel Amaya <miguel.amaya99@gmail.com>
2016-11-26 22:50:49 -0500

    Atrapando excepción de instancias no creadas.

--- almacen/settings.py
+++ almacen/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from almacen.models import TipoMovimiento, Almacen, Kardex
-PARAMETROS = (('F', 'POR FECHA',), ('M', 'POR MES',), ('A', 'POR AÑO',))
+PARAMETROS = (('F', 'POR FECHA',), ('M', 'POR MES',), ('A', 'POR Aï¿½O',))
 
 MESES = (
     ('01', 'ENERO'),
@@ -19,7 +19,20 @@ MESES = (
 
 FORMATOS = (('S', 'UNIDADES FISICAS',), ('V', 'VALORIZADO',))
 
-CHOICES_TIPOS_MOVIMIENTO = [(tm.codigo, tm.descripcion) for tm in TipoMovimiento.objects.all()]
-CHOICES_ALMACENES = [(alm.codigo, alm.descripcion) for alm in Almacen.objects.all()]
-CHOICES_MESES = [(str(mes.month).zfill(2), str(mes.month).zfill(2)) for mes in Kardex.objects.datetimes('fecha_operacion', 'month')]
-CHOICES_ANNIOS = [(anio.year, anio.year) for anio in Kardex.objects.datetimes('fecha_operacion', 'year')]
+try:
+    CHOICES_TIPOS_MOVIMIENTO = [(tm.codigo, tm.descripcion) for tm in TipoMovimiento.objects.all()]
+except:
+    CHOICES_TIPOS_MOVIMIENTO = []
+try:
+    CHOICES_ALMACENES = [(alm.codigo, alm.descripcion) for alm in Almacen.objects.all()]
+except:
+    CHOICES_ALMACENES = []
+
+try:
+    CHOICES_MESES = [(str(mes.month).zfill(2), str(mes.month).zfill(2)) for mes in Kardex.objects.datetimes('fecha_operacion', 'month')]
+except:
+    CHOICES_MESES = []
+try:
+    CHOICES_ANNIOS = [(anio.year, anio.year) for anio in Kardex.objects.datetimes('fecha_operacion', 'year')]
+except:
+    CHOICES_ANNIOS = []
