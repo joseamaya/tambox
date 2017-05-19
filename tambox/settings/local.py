40f7fae28dd1825ddb83d63acaf12f5f85642c4d
oliver <barsa_real_31@hotmail.com>
2017-05-18 22:44:08 -0500

    regresando archivo local a estado normal

--- tambox/settings/local.py
+++ tambox/settings/local.py
@@ -8,9 +8,9 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tambox',
-        'USER': 'oliver',
-        'PASSWORD': 'yugiho2000',
+        'NAME': 'tambox_dev',
+        'USER': 'tambox',
+        'PASSWORD': 's0p0rt3ccpp',
         'HOST': 'localhost',
         'PORT': '5432',
         'CHARSET': 'UTF8',
