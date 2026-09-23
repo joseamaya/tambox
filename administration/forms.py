ddd3ac61453cd85fecbdc6efafafa0b10e310a4b
Miguel Amaya <miguel.amaya99@gmail.com>
2026-09-23 17:46:28 -0500

    refactor(frontend): use Bootstrap modal and shared form mixin
    El modal reutilizable pasa al modal nativo de Bootstrap conservando la
    API openModal/closeModal. BootstrapFormMixin en tambox/forms.py aplica
    form-control/form-select/form-check-input e is-invalid, y los
    formularios dejan de repetir el bucle de clases en su __init__. La barra
    lateral marca la opcion activa y abre su submenu.

--- administration/forms.py
+++ administration/forms.py
@@ -1,121 +1,75 @@
 # -*- coding: utf-8 -*-
 from django import forms
 
+from tambox.forms import BootstrapFormMixin
 from tambox.widgets import NativeDateFieldsMixin
 from administration.models import Office, Worker, Position, Profession,\
     ApprovalLevel, Producer
 
 
-class ApprovalLevelForm(forms.ModelForm):
+class ApprovalLevelForm(BootstrapFormMixin, forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ApprovalLevelForm, self).__init__(*args, **kwargs)
+        self.fields['superior_level'].required = False
+
     class Meta:
         model = ApprovalLevel
         fields = ['description', 'superior_level']
 
-    def __init__(self, *args, **kwargs):
-        super(ApprovalLevelForm, self).__init__(*args, **kwargs)
-        self.fields['superior_level'].required = False
-        for field in iter(self.fields):
-            self.fields[field].widget.attrs.update({
-                'class': 'form-control'
-            })
 
-
-class ProfessionForm(forms.ModelForm):
+class ProfessionForm(BootstrapFormMixin, forms.ModelForm):
     class Meta:
         model = Profession
         fields = ['abbreviation', 'description']
 
+
+class OfficeForm(BootstrapFormMixin, forms.ModelForm):
     def __init__(self, *args, **kwargs):
-        super(ProfessionForm, self).__init__(*args, **kwargs)
-        for field in iter(self.fields):
-            self.fields[field].widget.attrs.update({
-                'class': 'form-control'
-            })
+        super(OfficeForm, self).__init__(*args, **kwargs)
+        self.fields['dependency'].required = False
 
-
-class OfficeForm(forms.ModelForm):
     class Meta:
         model = Office
         fields = ['code', 'name', 'dependency', 'is_management']
 
+
+class WorkerForm(BootstrapFormMixin, forms.ModelForm):
     def __init__(self, *args, **kwargs):
-        super(OfficeForm, self).__init__(*args, **kwargs)
-        for field in iter(self.fields):
-            if field != 'is_management':
-                self.fields[field].widget.attrs.update({
-                    'class': 'form-control'
-                })
-        self.fields['dependency'].required = False
+        super(WorkerForm, self).__init__(*args, **kwargs)
+        self.fields['profession'].required = False
 
-
-class WorkerForm(forms.ModelForm):
     class Meta:
         model = Worker
         fields = ['dni', 'last_name', 'first_name', 'profession', 'user', 'signature']
 
-    def __init__(self, *args, **kwargs):
-        super(WorkerForm, self).__init__(*args, **kwargs)
-        for field in iter(self.fields):
-            if field == 'dni':
-                self.fields[field].widget.attrs.update({
-                    'class': 'form-control entero'
-                })
-            else:
-                self.fields[field].widget.attrs.update({
-                    'class': 'form-control'
-                })
-        self.fields['profession'].required = False
 
-
-class ProducerForm(forms.ModelForm):
+class ProducerForm(BootstrapFormMixin, forms.ModelForm):
     class Meta:
         model = Producer
         fields = ['dni', 'last_name', 'first_name']
 
-    def __init__(self, *args, **kwargs):
-        super(ProducerForm, self).__init__(*args, **kwargs)
-        for field in iter(self.fields):
-            if field == 'dni':
-                self.fields[field].widget.attrs.update({
-                    'class': 'form-control entero'
-                })
-            else:
-                self.fields[field].widget.attrs.update({
-                    'class': 'form-control'
-                })
 
-
-class PositionForm(NativeDateFieldsMixin, forms.ModelForm):
-    class Meta:
-        model = Position
-        fields = ['name', 'office', 'worker', 'start_date', 'end_date', 'is_leadership', 'is_assistant']
-
+class PositionForm(BootstrapFormMixin, NativeDateFieldsMixin, forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(PositionForm, self).__init__(*args, **kwargs)
-        for field in iter(self.fields):
-            if field != 'is_leadership' and field != 'is_assistant':
-                self.fields[field].widget.attrs.update({
-                    'class': 'form-control'
-                })
         self.fields['end_date'].required = False
         self.fields['is_leadership'].required = False
         self.fields['is_assistant'].required = False
         self.fields['worker'].queryset = Worker.objects.exclude(
             pk__in=Position.objects.filter(end_date__isnull=True).values('worker'))
 
-
-class PositionUpdateForm(NativeDateFieldsMixin, forms.ModelForm):
     class Meta:
         model = Position
         fields = ['name', 'office', 'worker', 'start_date', 'end_date', 'is_leadership', 'is_assistant']
 
+
+class PositionUpdateForm(BootstrapFormMixin, NativeDateFieldsMixin, forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(PositionUpdateForm, self).__init__(*args, **kwargs)
-        for field in iter(self.fields):
-            if field != 'is_leadership' and field != 'is_assistant':
-                self.fields[field].widget.attrs.update({
-                    'class': 'form-control'
-                })
         self.fields['end_date'].required = False
         self.fields['is_leadership'].required = False
         self.fields['is_assistant'].required = False
+
+    class Meta:
+        model = Position
+        fields = ['name', 'office', 'worker', 'start_date', 'end_date', 'is_leadership', 'is_assistant']
